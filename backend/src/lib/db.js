f056b2cceed9d7201ec68e90bd2763607beeb16e
Your Name <you@example.com>
2026-02-05 16:32:06 +0530

    signup section code refactoring

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
+    if (!MONGO_URI) throw new Error('MONGO_URI is not defined in environment variables');
+    
     const conn = await mongoose.connect(process.env.MONGO_URI)
     console.log('MongoDB connected successfully', conn.connection.host);
   }catch (error) {
