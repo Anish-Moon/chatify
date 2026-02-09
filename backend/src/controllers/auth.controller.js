017dd39fd678bc8e59b94b53199f39ce002277e9
Your Name <you@example.com>
2026-02-09 20:57:44 +0530

    update profile endpoint added

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import User from "../models/User.js";
 import brrypt from "bcryptjs";
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
 import { ENV } from "../lib/env.js";
+import cloudinary from "../lib/cloudinary.js";
 
 export const signup =async (req,res)=> {
     const {fullName,email,password} = req.body;
@@ -64,6 +65,10 @@ export const signup =async (req,res)=> {
 export const login = async (req,res)=> {
     const {email,password} = req.body;
 
+    if(!email || !password){
+        return res.status(400).json({message: "Email and password are required"});
+    }
+
     try {
         const user = await User.findOne({email});
         if(!user) return res.status(400).json({message: "Invalid credentials"});
@@ -88,4 +93,22 @@ export const login = async (req,res)=> {
 export const logout =  (_,res)=> {
     res.cookie("jwt","",{maxAge : 0});
     res.status(200).json({message: "Logged out successfully"});
+}
+
+export const updateProfile = async (req,res) => {
+    try {
+        const {profilePic} = req.body;
+        if(!profilePic) return res.status(400).json({message: "Profile picture is required"});
+
+        const userId = req.user._id;
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic);
+
+        const updatedUser = await User.findByIdAndUpdate(userId, {profilePic: uploadResponse.secure_url}, {new: true});
+
+        res.status(200).json(updatedUser);
+        } catch (error) {
+        console.log("Error in updateProfile controller:", error);
+        res.status(500).json({message: "Internal Server error"});
+    }
 }
